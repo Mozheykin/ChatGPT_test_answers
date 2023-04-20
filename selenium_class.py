5d236140bb708d65e67ff220fa0d5c612a505851
Mozheykin <Mozheykin.igor@gmail.com>
2023-04-20 20:51:07 +0300

    feat(selenium) create class

--- selenium_class.py
+++ selenium_class.py
@@ -7,36 +7,107 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.remote.webelement import WebElement
 import time
 
 # https://chromedriver.chromium.org/help/chrome-doesn-t-start
 
-chrome_options = Options()
-# chrome_options.add_argument("--headless")
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=chrome_options)
 
-try:
-    driver.maximize_window()
-    driver.get('https://nurseslabs.com/nursing-pharmacology-nclex-practice-questions-test-bank/')
-    time.sleep(5)
-    Questions = driver.find_elements(By.CLASS_NAME, 'wpProQuiz_question_text')
-    for question in Questions:
-        if not question == '':
-            text = question.text
-            text = text.strip()
-            pprint(text)
-    # Answers = driver.find_elements(By.CLASS_NAME, 'wpProQuiz_questionInput')
-    # for answer in Answers:
-    #     pprint(answer.tag_name)
-    # button = driver.find_element(By.NAME, 'check')
-    # button.send_keys(Keys.ARROW_DOWN)
-    # with open('temp.hml', 'w') as file:
-    #     file.write(driver.page_source)
-    time.sleep(5)
-except Exception as ex:
-    print(ex)
-finally:
-    driver.close()
-    driver.quit()
+
+class SeleniumParse:
+    def __init__(self, options_arguments:list) -> None:
+        chrome_options = Options()
+        for option in options_arguments:
+            chrome_options.add_argument(option)
+        # chrome_options.add_argument("--headless")
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=chrome_options)
+        self.driver.maximize_window()
+
+    def get_class_text(self, url:str, class_names:list):
+        try:
+            self.driver.get(url)
+            result = dict()
+            for class_name in class_names:
+                self.wait_until_visible(class_name=class_name)
+                result[class_name] = self.get_find_elements(class_name)
+            return result
+            # button = driver.find_element(By.NAME, 'check')
+            # button.send_keys(Keys.ARROW_DOWN)
+            # with open('temp.hml', 'w') as file:
+            #     file.write(driver.page_source)
+        except Exception as ex:
+            print(ex)
+            self.driver.close()
+            self.driver.quit()
+        
+    def wait_until_clickable(self, xpath :str=None, css_selector=None, class_name :str=None, _id :str=None, duration :int=10000, frequency :float=0.01):
+        if xpath:
+            WebDriverWait(self.driver, duration, frequency).until(EC.element_to_be_clickable((By.XPATH, xpath)))
+        elif class_name:
+            WebDriverWait(self.driver, duration, frequency).until(EC.element_to_be_clickable((By.CLASS_NAME, class_name)))
+        elif css_selector:
+            WebDriverWait(self.driver, duration, frequency).until(EC.element_to_be_clickable((By.CSS_SELECTOR, css_selector)))
+        elif _id:
+            WebDriverWait(self.driver, duration, frequency).until(EC.element_to_be_clickable((By.ID, _id)))
+    
+    def wait_until_visible(self, xpath :str=None, css_selector=None, class_name :str=None, _id :str=None, duration :int=10000, frequency :float=0.01):
+        if xpath:
+            WebDriverWait(self.driver, duration, frequency).until(EC.visibility_of_element_located((By.XPATH, xpath)))
+        elif class_name:
+            WebDriverWait(self.driver, duration, frequency).until(EC.visibility_of_element_located((By.CLASS_NAME, class_name)))
+        elif css_selector:
+            WebDriverWait(self.driver, duration, frequency).until(EC.visibility_of_element_located((By.CSS_SELECTOR, css_selector)))
+        elif _id:
+            WebDriverWait(self.driver, duration, frequency).until(EC.visibility_of_element_located((By.ID, _id)))
+        
+    def get_find_elements(self, class_name:str) -> list:
+        elements= self.driver.find_elements(By.CLASS_NAME, class_name)
+        result = list()
+        for element in elements:
+            text = element.text.strip()
+            if text:
+                result.append(text)
+        return result
+
+    def find_element(self, xpath:str=None, partial_link_text:str=None, css_selector:str=None) -> WebElement:
+        if xpath:
+            return self.driver.find_element(By.XPATH, xpath)
+        elif partial_link_text:
+            return self.driver.find_element(By.PARTIAL_LINK_TEXT, partial_link_text)
+        elif css_selector:
+            return self.driver.find_element(By.CSS_SELECTOR, css_selector)
+    
+    def scroll_element(self, element:WebElement):
+        ActionChains(self.driver).move_to_element(element).perform()
+        
+
+    def close(self):
+        self.driver.close()
+        self.driver.quit()
+
+
+if __name__ == '__main__':
+    options_arguments = ['--no-sandbox', '--disable-dev-shm-usage']
+    url = 'https://nurseslabs.com/nursing-pharmacology-nclex-practice-questions-test-bank/'
+    test_parse = SeleniumParse(options_arguments=options_arguments)
+    question_class = 'wpProQuiz_question_text'
+    answers_class = 'wpProQuiz_questionListItem'
+    class_names = [question_class, answers_class]
+
+    result = test_parse.get_class_text(url=url, class_names=class_names)
+    print(f'{result=}')
+
+    # test_parse.wait_until_clickable(xpath='//*[@id="wpProQuiz_62"]') 
+    button = test_parse.find_element(xpath='//*[@id="wpProQuiz_62"]')
+    test_parse.scroll_element(element=button)
+
+
+    select_answer = test_parse.find_element(xpath='//*[@id="wpProQuiz_62"]')
+    select_answer = select_answer.find_element(By.ID, result[answers_class][1])
+    select_answer.click()
+
+    time.sleep(15)
+    test_parse.close()
